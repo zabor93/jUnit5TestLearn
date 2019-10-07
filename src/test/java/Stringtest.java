1b52f075e60f5456e8e1e1424bc2c2d72bccbf71
Jakub Zaboronek <jakubzaboronek@wp.pl>
2019-10-07 10:12:43 +0200

    Added test such as beforeEach,afterAll etc

--- src/test/java/Stringtest.java
+++ src/test/java/Stringtest.java
@@ -1,18 +1,39 @@
-import org.junit.Test;
 
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class Stringtest {
 
+    @BeforeAll
+    static void beforeAll(){
+        System.out.println("Initialize connection to database");
+    }
+
+    @AfterAll
+    static void afterAll(){
+        System.out.println("Close connection to database");
+    }
+
+    @BeforeEach
+    void beforeEach(TestInfo info){
+        System.out.println("Initialize Test data for:"+info.getDisplayName());
+    }
+
+    @AfterEach
+    void afterEach(){
+        System.out.println("CLEAN UP");
+    }
+
     @Test
-    public void whateveryouwant() {
+     void whateveryouwant() {
         int actualLength = "ABCD".length();
         int expectedLength = 4;
         assertEquals(expectedLength, actualLength);
     }
 
     @Test
-    public void toUpperCase(){
+     void toUpperCase(){
         String str = "abcd";
         String result = str.toUpperCase();
         assertNotNull(result);
@@ -20,7 +41,7 @@ public class Stringtest {
     }
 
     @Test
-    public void contains_basic() {
+     void contains_basic() {
         String str = "abcdefgh";
         boolean result = str.contains("ijk");
         assertEquals(false, result);
