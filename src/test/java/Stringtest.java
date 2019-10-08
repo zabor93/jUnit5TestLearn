ffe06c29e4c9af3cc85c218f4003aad98d2f635a
Jakub Zaboronek <jakubzaboronek@wp.pl>
2019-10-08 09:59:54 +0200

    Added parametrized test and how to test exception

--- src/test/java/Stringtest.java
+++ src/test/java/Stringtest.java
@@ -1,5 +1,7 @@
 
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -55,4 +57,27 @@ public class Stringtest {
         String[] expectedResult = new String[]{"abc", "def", "ghi"};
         assertArrayEquals(expectedResult, actualResult);
     }
+    @Test
+    @DisplayName("When length is null, throw an exception")
+    void testExceptionNullPointer() {
+        String str = null;
+        assertThrows(NullPointerException.class,()->{str.length();});
+    }
+
+    @Test
+    void length_greater_than_zero(){
+        assertTrue("ABCFD".length() > 0);
+        assertTrue("ACFD".length() > 0);
+        assertTrue("AD".length() > 0);
+        assertTrue("A".length() > 0);
+        assertTrue("AD".length() > 0);
+        assertTrue("AdsdsdsBCFD".length() > 0);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"abc", "ABCDS", "A"})
+    void length_greater_than_zero_using_parameterized_test(String string) {
+        assertTrue(string.length() > 0);
+    }
+
 }
